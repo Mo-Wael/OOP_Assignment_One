699839247637c7f7514362eaf53f1dbb813854bb
abdelrahman70ssam <115511118+abdelrahman70ssam@users.noreply.github.com>
2022-10-25 18:06:32 +0200

    Update A1_SheetPb6_20210201.cpp

--- A1_SheetPb6_20210201.cpp
+++ A1_SheetPb6_20210201.cpp
@@ -6,42 +6,34 @@
 using namespace std ;
 
 #define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
-void fractal( unsigned int n, unsigned int i = 0  ){
-if (n == 2){
-        cout << "*\n* *\n  *\n" ; 
-    }
 
-    else{
-       
-        fractal( n / 2);
-
-        for (int k = 0; k < n; k++){
-            cout << "* ";
-        }
-
-        cout << endl;
-        
-        for (int k = 0; k <= n; k++){
-            cout << ' ';
-            }
-        cout << '*' << endl; 
-          for (int k = 0; k <= n; k++){
-            cout << ' ';
-            }
-        cout << "* *" << endl ;
-            for (int k = 0; k <= n+2; k++){
-                cout << ' ';
-            }
-        cout << "*" << endl ;
-        
-        
-    }
+string pattern(string outs, unsigned int n, unsigned int i) 
+{
+       int temp = (int) (log(n) / log(2));
+       if (temp == 0) 
+       {
+           for (int j = 0; j < i; j++)
+               outs += ' ';
+           for (int j = 0; j < n; j++)
+               outs += "* ";
+           return outs += '\n';
+       } 
+       else 
+       {
+           outs = pattern(outs, (int) (pow(2.0, temp - 1)), i);
+           for (int j = 0; j < i; j++)
+               outs += ' ';
+           for (int j = 0; j < n; j++)
+               outs += "* ";
+           outs += '\n';
+           i = i + temp * 2;
+           return outs = pattern(outs, (int) (pow(2.0, temp - 1)), i);
+       }
 }
 
-int main()
-{
-	fast 
-    fractal(8 , 0)	;
-	 
-	return 0 ;
+int main() {
+  
+cout << pattern("", 8, 0) << endl;
+
+return 0;
 }
